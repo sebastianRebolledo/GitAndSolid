46c914d86a4c074d512bcd9b991542d9d041f506
ENDAVA\abonilla <alexis.bonilla@endava.com>
2021-07-29 15:32:07 -0500

    "Commit the S class in de S branch and testing the functionality of SourceTree"

---
+++ src/main/java/S/CorrectUse.java
@@ -0,0 +1,168 @@
+package S;
+
+import java.util.ArrayList;
+
+public class CorrectUse {
+
+	
+	public CorrectUse() {
+		
+		
+		Rectangle r = new Rectangle(20, 10);
+		Circle c = new Circle(5);
+		
+		ArrayList<Shape> array = new ArrayList<Shape>();
+		
+		array.add(r);
+		array.add(c);
+		
+		ShapeDetails shapeDetails= new ShapeDetails();
+		shapeDetails.setShapes(array);
+		shapeDetails.printDetails();
+		
+		
+		
+		
+	}
+	
+	
+
+	
+	
+	
+	public static void main(String[] args) {
+		CorrectUse correctUse = new CorrectUse();
+		
+		
+		
+		
+
+	}
+	
+	
+	
+	class ShapeDetails{
+		
+		private ArrayList<Shape> shapes;
+		
+		public ShapeDetails() {
+			this.shapes=new ArrayList<Shape>();
+		}
+		
+		public void setShapes(ArrayList<Shape> shapes) {
+			this.shapes = shapes;
+		}
+		
+		public ArrayList<Shape> getShapes(){
+			return this.shapes;
+			
+			
+		}
+		
+		public void printDetails() {
+			for (Shape shape : shapes) {
+			
+				System.out.println("Area: "+ shape.area + " Perimeter: "+shape.perimeter + " Type: "+shape.getClass().getSimpleName());
+			}
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	class Shape {
+		
+		
+		
+		
+		int area;
+		int perimeter;
+		
+		
+		public Shape( ) {
+			this.area = 0;
+			this.perimeter = 0;
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	class Rectangle extends Shape{
+		
+		int height;
+		int width;
+		
+		public Rectangle(int height, int width) {
+			this.height = height;
+			this.width = width;
+			this.area=getArea();
+			this.perimeter=getPerimeter();
+		}
+		
+		public int getArea() {
+			
+			return this.height*this.width;
+		}
+		
+		
+		
+		public int getPerimeter() {
+			return 2*(this.width+this.height);
+		}
+		
+		
+		
+		
+		
+	}
+	
+	class Circle extends Shape{
+		
+		int radius;
+	
+		
+		public Circle(int radius) {
+			this.radius = radius;
+			this.area=getArea();
+			this.perimeter=getPerimeter();
+		
+			
+		}
+		
+		public int getArea() {
+			
+			return (int)(Math.PI*(this.radius*this.radius));
+		}
+		
+		
+		
+		public int getPerimeter() {
+			return (int)(Math.PI*this.radius*2);
+		}
+		
+		
+		
+		
+		
+	}
+	
+
+}
---
+++ src/main/java/S/IncorrectUse.java
@@ -0,0 +1,129 @@
+package S;
+
+import java.util.ArrayList;
+
+import S.CorrectUse.Circle;
+import S.CorrectUse.Rectangle;
+import S.CorrectUse.Shape;
+import S.CorrectUse.ShapeDetails;
+
+public class IncorrectUse {
+	
+	
+	
+
+	public IncorrectUse() {
+		
+		
+		Rectangle r = new Rectangle(20, 10);
+		Circle c = new Circle(5);
+		
+		ArrayList<Shape> array = new ArrayList<Shape>();
+		
+		array.add(r);
+		array.add(c);
+		
+		
+		for (Shape shape : array) {
+			
+			System.out.println("Area: "+ shape.area + " Perimeter: "+shape.perimeter + " Type: "+shape.getClass().getSimpleName());
+		}
+
+	}
+	
+
+	public static void main(String[] args) {
+		IncorrectUse incorrectUse = new IncorrectUse();
+
+	}
+	
+	
+	
+	class Shape {
+		
+		
+		
+		
+		int area;
+		int perimeter;
+		
+		
+		public Shape( ) {
+			this.area = 0;
+			this.perimeter = 0;
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	class Rectangle extends Shape{
+		
+		int height;
+		int width;
+		
+		public Rectangle(int height, int width) {
+			this.height = height;
+			this.width = width;
+			this.area=getArea();
+			this.perimeter=getPerimeter();
+		}
+		
+		public int getArea() {
+			
+			return this.height*this.width;
+		}
+		
+		
+		
+		public int getPerimeter() {
+			return 2*(this.width+this.height);
+		}
+		
+		
+		
+		
+		
+	}
+	
+	class Circle extends Shape{
+		
+		int radius;
+	
+		
+		public Circle(int radius) {
+			this.radius = radius;
+			this.area=getArea();
+			this.perimeter=getPerimeter();
+		
+			
+		}
+		
+		public int getArea() {
+			
+			return (int)(Math.PI*(this.radius*this.radius));
+		}
+		
+		
+		
+		public int getPerimeter() {
+			return (int)(Math.PI*this.radius*2);
+		}
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
